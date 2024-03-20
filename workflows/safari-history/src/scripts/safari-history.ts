71a92063c96461e7232e219cf4c040187751bee5
Robbie Gates <robbie@halfyak.org>
2024-03-21 07:56:38 +1100

    Include history from alternate profiles
    Safari keeps a separate History.db for each alternate profile, so search
    those also. This approach won't perform well for many profiles, but for
    my current use case of default personal and one for work, it should be
    ok.

--- workflows/safari-history/src/scripts/safari-history.ts
+++ workflows/safari-history/src/scripts/safari-history.ts
@@ -4,7 +4,30 @@ import { DetailedError, executeProcess } from "@halfyak/alfred-workflows-jxa";
 run = (argv: string[]) => {
   try {
     const home = $.NSHomeDirectory().js;
-    const safariHistoryDb = `${home}/Library/Safari/History.db`;
+    const defaultHistoryDb = `${home}/Library/Safari/History.db`;
+    // Safari profiles have separate dbs. Also not every directory here has a
+    // History.db for me, so silently ignore directories without a db.
+    const safariProfilesRoot = `${home}/Library/Containers/com.apple.Safari/Data/Library/Safari/Profiles`;
+    const error = $();
+    const profileDirs =
+      $.NSFileManager.defaultManager.contentsOfDirectoryAtPathError(
+        safariProfilesRoot,
+        error,
+      ).js;
+    if (undefined === profileDirs) {
+      throw new Error(
+        `contentsOfDirectoryAtPathError (${error.code}): ${error.localizedDescription.js}`,
+      );
+    }
+    const profileHistoryDbs = profileDirs
+      .map(ObjC.unwrap)
+      .flatMap((profileName: string) => {
+        const profileHistoryDb = `${safariProfilesRoot}/${profileName}/History.db`;
+        return $.NSFileManager.defaultManager.fileExistsAtPath(profileHistoryDb)
+          ? [profileHistoryDb]
+          : [];
+      });
+
     const whereClause = () =>
       0 == argv.length
         ? ""
@@ -18,14 +41,36 @@ run = (argv: string[]) => {
       ORDER BY history_visits.visit_time DESC
       LIMIT 10;`;
 
-    const jsonResult = executeProcess(
-      "/usr/bin/sqlite3",
-      sql,
-      "-readonly",
-      safariHistoryDb,
-      "-batch",
-      "-json",
-    );
+    // Consider prioritising results from the most recent browser session:
+    // `defaults read com.apple.Safari SafariProfilesLastActiveProfileUUIDString`
+    // which is either a UUID or `DefaultProfile`. Also, this grabs 10 per
+    // profile linearly in the profile, which will be slow for many profiles.
+    // Maybe there should be a mode that only checks default and most recent if
+    // not default?
+    const historyItems = [defaultHistoryDb, ...profileHistoryDbs]
+      .flatMap((safariHistoryDb) => {
+        const jsonResult = executeProcess(
+          "/usr/bin/sqlite3",
+          sql,
+          "-readonly",
+          safariHistoryDb,
+          "-batch",
+          "-json",
+        );
+        return "" === jsonResult
+          ? // When sqlite finds nothing in JSON mode, it emits nothing, as opposed to
+            // an empty list.
+            []
+          : // The as here couples correctness to the sql query above and the
+            // behaviour of sqlite. It'd be nicer to be careful but historically
+            // it hasn't been a source of problems.
+            (JSON.parse(jsonResult) as Array<{
+              title: string;
+              url: string;
+              visit_time: number;
+            }>);
+      })
+      .toSorted((left, right) => right.visit_time - left.visit_time);
     const dateFormatter = $.NSDateFormatter.alloc.init;
     dateFormatter.dateStyle = $.NSDateFormatterShortStyle;
     dateFormatter.timeStyle = $.NSDateFormatterShortStyle;
@@ -39,17 +84,10 @@ run = (argv: string[]) => {
       dateFormatter.stringFromDate(
         $.NSDate.dateWithTimeIntervalSinceReferenceDate(time),
       ).js;
-    // When sqlite finds nothing in JSON mode, it emits nothing, as opposed to an empty list.
     const items =
-      "" === jsonResult
+      0 === historyItems.length
         ? [{ title: "Nothing matching", valid: false }]
-        : (
-            JSON.parse(jsonResult) as Array<{
-              title: string;
-              url: string;
-              visit_time: number;
-            }>
-          ).map(({ title, url, visit_time }) => ({
+        : historyItems.map(({ title, url, visit_time }) => ({
             title,
             // osascript does not appear to provide the standard URL class. We
             // could use NSURL, but for now this parsing, a string split, is ok.
